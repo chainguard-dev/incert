cb644c95890c787d3c0d132371ce13a240e4a19b
Rob Best <rob.best@chainguard.dev>
2025-06-02 18:02:09 +0100

    Add support for multi-arch images
    Presently, when `incert` encounters a multi-arch image it will resolve
    it down to the platform specific manifest (by default linux/amd64) and
    insert certificates into that. A single manifest is published to the
    destination.
    
    With this change, `incert` will insert the certificates into each image
    in the list and publish a new index to the destination, derived from the
    source.
    
    I think this matches the user's expectation of how the command would behave and
    removes some toil for users who are interested in actually consuming
    multiple architectures.
    
    This changes the default behaviour of `incert` when it encounters a
    multi-arch image and could be considered a breaking change.
    
    The alternative would be to put this behind a `--recursive` flag or
    something but my feeling is that this is the expected default behaviour.
    
    Signed-off-by: Rob Best <rob.best@chainguard.dev>

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@
 # Assumes incert is at "../incert" and docker is available
 
 set -e
-#set -x
+set -x
 
 cleanup() { 
   docker rm -f $NGINX
@@ -29,12 +29,25 @@ NGINX=$(docker run -p 8443:8443 -d \
 # check insecure curl works
 docker run --rm -it --network host --add-host example.com:127.0.0.1 cgr.dev/chainguard/curl:latest-dev -k https://example.com:8443
 
-# now create container with cert and try
+# now create container with cert and try; this should produce an image index
 IMAGE=ttl.sh/incert/test-default-$RANDOM:20m
 ../incert --ca-certs-file selfsigned.pem --image-url cgr.dev/chainguard/curl:latest --dest-image-url $IMAGE
 docker run --rm -it --network host --add-host example.com:127.0.0.1 $IMAGE https://example.com:8443
+docker manifest inspect $IMAGE | docker run --rm -i --entrypoint jq cgr.dev/chainguard/curl:latest-dev -e '.mediaType == "application/vnd.oci.image.index.v1+json"'
 
-# test using platform argument
+# test using platform argument; this should produce an image manifest
 IMAGE=ttl.sh/incert/test-arm64-default-$RANDOM:20m
 ../incert --ca-certs-file selfsigned.pem --image-url cgr.dev/chainguard/curl:latest --platform linux/arm64 --dest-image-url $IMAGE
+docker run --rm -it --network host --add-host example.com:127.0.0.1 $IMAGE https://example.com:8443
+docker manifest inspect $IMAGE | docker run --rm -i --entrypoint jq cgr.dev/chainguard/curl:latest-dev -e '.mediaType == "application/vnd.oci.image.manifest.v1+json"'
+
+# test against a specific manifest, rather than an index
+IMAGE=ttl.sh/incert/test-image-$RANDOM:20m
+CURL_DIGEST=$(docker manifest inspect cgr.dev/chainguard/curl:latest | docker run --rm -i --entrypoint jq cgr.dev/chainguard/curl:latest-dev -r '.manifests[] | select(.platform.architecture == "arm64" and .platform.os == "linux") | .digest')
+../incert --ca-certs-file selfsigned.pem --image-url cgr.dev/chainguard/curl@$CURL_DIGEST --dest-image-url $IMAGE
+docker run --rm -it --network host --add-host example.com:127.0.0.1 $IMAGE https://example.com:8443
+
+# for manifests, the --platform flag should make no difference
+IMAGE=ttl.sh/incert/test-image-amd64-$RANDOM:20m
+../incert --ca-certs-file selfsigned.pem --image-url cgr.dev/chainguard/curl@$CURL_DIGEST --platform=linux/amd64 --dest-image-url $IMAGE
 docker run --rm -it --network host --add-host example.com:127.0.0.1 $IMAGE https://example.com:8443
